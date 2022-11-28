2fae373dee512de25e0ee134766d23d07d9647c4
unknown <OverflowDev>
2022-11-28 23:59:16 +0100

    project update

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -9,27 +9,35 @@ const Projects = [
     }, 
     {
         id: 2,
+        name: 'Overflow Market',
+        description: 'Overflow Market is an e-commerce website that allows you to add products to cart and pay with any crypto currency of your choice',
+        url: 'https://overflowmarket.vercel.app/',
+        image: 'https://images.pexels.com/photos/953862/pexels-photo-953862.jpeg?cs=srgb&dl=pexels-emirkhan-bal-953862.jpg&fm=jpg'
+        
+    }, 
+    {
+        id: 3,
         name: 'Interior',
         description: 'Interior is an interior website that allows to know more about interiors',
         url: 'https://interior-xi.vercel.app/',
         image: 'https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?cs=srgb&dl=pexels-vecislavas-popa-1643383.jpg&fm=jpg'
     }, 
     {
-        id: 3,
+        id: 4,
         name: 'Github Finder',
         description: 'Github Finder is a tool that uses GitHub API to fetch any users on Github',
         url: 'https://github-finder-app-drab-two.vercel.app/',
         image: 'https://images.pexels.com/photos/4578665/pexels-photo-4578665.jpeg?cs=srgb&dl=pexels-markus-winkler-4578665.jpg&fm=jpg'
     }, 
     {
-        id: 4,
+        id: 5,
         name: 'House Marketplace ',
         description: 'House Marketplace is marketplace that allows you to rent and buy house ',
         url: 'https://house-marketplace-chi-seven.vercel.app/',
         image: 'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?cs=srgb&dl=pexels-expect-best-323780.jpg&fm=jpg'
     }, 
     {
-        id: 5,
+        id: 6,
         name: 'Weather App',
         description: 'Weather App is the app to check any current weather ',
         url: 'https://weatherapp-85n67a65x-overflowdev.vercel.app/',
